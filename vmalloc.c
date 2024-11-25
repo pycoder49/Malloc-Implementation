dd296b76b49da2f01262af15553a0a1ffaac2b21
Aryan Ahuja <75223447+pycoder49@users.noreply.github.com>
2024-11-25 20:04:50 +0000

    test

--- vmalloc.c
+++ vmalloc.c
@@ -155,5 +155,6 @@ void *vmalloc(size_t size)
     assert((uintptr_t)((char *)best_fit_block + sizeof(struct block_header)) % BLKALIGN == 0);
     printf("\n\nhello\n\n");
 
-    return (void *)((char *)best_fit_block + sizeof(struct block_header));
+    //return (void *)((char *)best_fit_block + sizeof(struct block_header));
+    return NULL;
 }
